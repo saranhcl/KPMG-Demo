e844011d24de82a1193a4e5785b9f24ad369c7b4
saranhcl <33221711+saranhcl@users.noreply.github.com>
2022-06-13 10:29:01 +0530

    Create nested_object.py
    challenge-3

---
+++ nested_object.py
@@ -0,0 +1,21 @@
+myobj = {
+   "obj1" : {
+     "x" : "a",
+     "y" : "b"
+   },
+    "obj2" : {
+      "x" : "a",
+      "y" : "b"
+    },
+    "obj3" : {
+      "x" : "a",
+      "y" : "b"
+    }
+}
+
+
+print(myobj)
+
+print(myobj.get("obj1"))
+print(myobj.get("obj2"))
+print(myobj.get("obj3"))
